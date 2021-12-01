efd1379b79255b24078e07963b5857ab80d8f0c5
Elard Burga Goicochea <elard3d@hotmail.com>
2021-12-01 08:01:54 -0500

    botones creados

---
+++ app/src/main/java/com/example/appaccounts/AccountsDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.appaccounts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accounts_detail);
+    }
+}
